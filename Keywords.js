c0b9c9af15e7296b7033c0c00ef7752dda1eb18f
Gopal <gopalmittal2003@gmail.com>
2024-11-29 19:55:12 +0530

    first commit

---
+++ Keywords.js
@@ -0,0 +1,16 @@
+console.log("Test using node in terminal of the vs code") //Done
+
+//Keywords
+
+const accountId = "1233";
+let accountHolderName = "Gopal";
+var accountHolderCity = "Hyderabad";
+
+console.log(accountId);
+console.log(accountHolderName);
+console.log(accountHolderCity);
+
+console.table([accountId, accountHolderName,accountHolderCity]);
+
+console.log(window);
+
