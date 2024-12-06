42ef8478125ea0775cff87926f10fd9a3277256e
Gopal <gopalmittal2003@gmail.com>
2024-12-06 23:11:58 +0530

    Strings

---
+++ Strings.js
@@ -0,0 +1,25 @@
+// console.log("cat".charAt(1));
+// console.log("cat"[1]);
+
+const str1 = "2 + 2";
+const str2 = String(2 + 2);
+const str3 = new String("Gopal Agarwal");
+
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// console.log(eval(str1))
+// console.log(eval(str2))
+// console.log(eval(str3))
+
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(typeof str3);
+
+// console.log(str1.charAt(0));
+
+console.log(str3)
+
+
